ca5a90c401a6e61b93b7ae0c0a0085991318c5e8
Ivan Tavares - TJ <ivan.tavares@tjro.jus.br>
2023-09-05 13:35:25 -0400

    CommitZeroTres

--- src/FomatacaoTexto.java
+++ src/FomatacaoTexto.java
@@ -4,5 +4,6 @@ public class FomatacaoTexto {
         int idade = 30;
         double valor = 55.9999;
         System.out.println(String.format("Meu nome é %s, eu tenho %d anos de idade e hoje eu gastei %.2f", nome, idade, valor));
+
     }
 }
--- src/teste.java
+++ src/teste.java
@@ -2,5 +2,6 @@ public class teste {
     public static void main(String[] args){
 
         System.out.println("TEste1");
+        System.out.println("Teste 2.1 git");
     }
 }
